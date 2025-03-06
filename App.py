844bdb58d8c502a8cbd5a18148ea48fc9b1a8209
Sudhanshu Sanganeria <122626095+Sudhanshusng@users.noreply.github.com>
2025-03-06 18:17:21 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,27 @@
+import streamlit as st
+from transformers import pipeline
 
+@st.cache_resource
+def load_sentiment_analyser():
+    return pipeline("sentiment-analysis")
+
+def main():
+    try:
+        st.title("Sentiment Analyser")
+        st.markdown(''' This app analyses your feelings based on the input text given by the user''')
+        input = st.text_area("Enter your input Text" , height=200)
+        analyse_button = st.button("Analyse Text")
+        if analyse_button:
+         if input.strip() :
+            sentiment_analyser = load_sentiment_analyser()
+            result = sentiment_analyser(input)
+            st.write(result)
+        
+         else:
+           st.error("INVALID TEXT")
+    
+    except Exception as e:
+        st.error(e)
+
+if __name__=="__main__":
+    main()
